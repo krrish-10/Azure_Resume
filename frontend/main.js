138ca9353e6546271279c24434c24852fa21cf72
krrish-10 <54485676+krrish-10@users.noreply.github.com>
2021-08-13 15:29:54 +0530

    Added JS File

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentloaded", (event) => {
+    getVisitCount();
+})
+const functionApi = '';
+
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called Function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+
+    }).catch(function (error) {
+
+        console.log(error)
+
+    })
+}
