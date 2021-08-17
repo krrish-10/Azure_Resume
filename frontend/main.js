e9b44338193ee2ed90aaecc587fda4c47c628475
krrish-10 <54485676+krrish-10@users.noreply.github.com>
2021-08-17 23:09:53 +0530

    Deployed Storage

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,22 @@
-window.addEventListener("DOMContentloaded", (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+});
+const functionApiUrl = 'https://getresumecountermk.azurewebsites.net/api/GetResumeCounter?code=Gr9l0l3WEak5kFZ0zmgweXAictHY4xSDBnIiNzhsawu1KlRX5APalA==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl)
+        .then(response => {
         return response.json()
-    }).then(response => {
-        console.log("Website called Function API.");
+        })
+        .then(response => {
+            console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
-
+            document.getElementById('counter').innerText = count;
     }).catch(function (error) {
-
-        console.log(error)
-
-    })
+        console.log(error);
+    });
+    return count;
 }
